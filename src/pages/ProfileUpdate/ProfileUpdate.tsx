54b36fc96bc567da31cb420bca74c19a73777f8a
Great Lucky <lgreat098@gmail.com>
2026-05-23 14:49:08 +0100

    implement profile page

--- src/pages/ProfileUpdate/ProfileUpdate.tsx
+++ src/pages/ProfileUpdate/ProfileUpdate.tsx
@@ -1,5 +1,39 @@
+import { useState } from "react";
+import "./ProfileUpdate.css";
+
 const ProfileUpdate = () => {
-  return <div>ProfileUpdate</div>;
+  const [image, setImage] = useState<File | null>(null);
+  return (
+    <div className="profile">
+      <div className="profile-container">
+        <form>
+          <h3>Profile Details</h3>
+          <label htmlFor="avatar">
+            <input
+              onChange={(e) => setImage(e.target.files?.[0] || null)}
+              type="file"
+              id="avatar"
+              accept=".png, .jpeg, .jpg"
+              hidden
+            />
+            <img
+              src={image ? URL.createObjectURL(image) : "/profile-picture.png"}
+              alt="profile"
+            />
+            upload profile image
+          </label>
+          <input type="text" placeholder="Your name" required />
+          <textarea placeholder="Write profile bio" required></textarea>
+          <button type="submit">Save</button>
+        </form>
+        <img
+          className="profile-image"
+          src={image ? URL.createObjectURL(image) : "/login-logo.svg"}
+          alt="logo"
+        />
+      </div>
+    </div>
+  );
 };
 
 export { ProfileUpdate };
